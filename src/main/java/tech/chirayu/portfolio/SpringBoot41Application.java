9c605a37484e9b647d8ba0f462547f0592332ca6
ChirayuP19 <chirayu010675@gmail.com>
2025-12-24 23:00:52 -0500

    REST API starting

---
+++ src/main/java/tech/chirayu/portfolio/SpringBoot41Application.java
@@ -0,0 +1,13 @@
+package tech.chirayu.portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot41Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot41Application.class, args);
+	}
+
+}
